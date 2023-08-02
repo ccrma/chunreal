808c23b8d58070dbab12ddaf20fd6139bfcf02e2
emurakam <75334216+emurakam@users.noreply.github.com>
2023-08-02 15:37:08 -0700

    add utility shell scripts

---
+++ scripts/clean.sh
@@ -0,0 +1,4 @@
+CHUNREAL_PROJECT="$(pwd)/../Chunreal_Project"
+
+rm -rf ${CHUNREAL_PROJECT}/.vsconfig ${CHUNREAL_PROJECT}/.vs/ ${CHUNREAL_PROJECT}/Binaries/ ${CHUNREAL_PROJECT}/DerivedDataCache/ ${CHUNREAL_PROJECT}/Intermediate/ ${CHUNREAL_PROJECT}/Saved/ ${CHUNREAL_PROJECT}/Plugins/Chunreal/Binaries/ ${CHUNREAL_PROJECT}/Plugins/Chunreal/Intermediate ${CHUNREAL_PROJECT}/Saved/Logs/ 
+rm -f ${CHUNREAL_PROJECT}/Chunreal_Project.sln
